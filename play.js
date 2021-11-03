5dacae0f5c6a7a19e71a5764e8a13a0e207cbf50
Nasser Mokadem <nmokadem@hotmail.com>
2021-11-03 19:13:07 +0000

    inital commit for play.js

---
+++ play.js
@@ -0,0 +1,81 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Connecting ...");
+  });
+
+  conn.on('data', function(message){ // this is how we receive
+    console.log(`Server says: ${message}`);
+  });
+
+  
+  return conn;
+};
+
+connect();
+
+
+
+// const net = require('net');
+// const port = 50541;  //8009;
+
+// const configObj = {
+// port: port,
+// host: 'localhost'
+// };
+
+// const client = net.createConnection(configObj);
+
+// client.on('connect',function(){
+// console.log('client is connected to server.');
+// });
+
+// client.on('data', function(message){ // this is how we receive
+// console.log(`Server says: ${message}`);
+// });
+
+// process.stdin.on('data', function(message){ // this is how we send
+// client.write(message);
+// });
+
+// client.on('end', function(){
+// console.log('client is disconnected from the server.');
+// }); 
+
+//----------------------------------------------------------
+
+// const net = require('net');
+// const port = 14740;
+
+// const configObj = {
+// port: port,
+// host: '4.tcp.ngrok.io'
+// };
+
+// const client = net.createConnection(configObj);
+
+// client.on('connect',function(){
+// console.log('client is connected to server.');
+// });
+
+// client.on('data', function(message){ // this is how we receive
+// console.log(`Server says: ${message}`);
+// });
+
+// process.stdin.on('data', function(message){ // this is how we send
+// client.write(message);
+// });
+
+// client.on('end', function(){
+// console.log('client is disconnected from the server.');
+// }); 
